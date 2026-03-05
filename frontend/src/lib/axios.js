a8c133afeef12cf8c88a2075ce1db4b645f6049c
Karan Mijhar <chmcs2026.karan.mijhar@gmail.com>
2026-03-05 19:25:44 +0530

    connected frontend to render backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://mern-product-management-system-backend.onrender.com/"
 });
 
 export default axiosInstance;
